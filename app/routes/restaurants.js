9e191e89126c68c98826081ac1574ea13ba8b3fb
Anthony Trama <AnthonyT@ashn.com>
2017-11-17 15:16:33 -0800

    session2 work

--- app/routes/restaurants.js
+++ app/routes/restaurants.js
@@ -2,6 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(){
-        return ['McDonalds', 'Rubios', 'Pizza Hut']
+        let arr = [
+            {
+                name:'McDonalds', 
+                price: 1, 
+                favorite: true
+            }, 
+            {
+                name:'Rubios', 
+                price: 3, 
+                favorite: false
+            },
+            {
+                name:'Pizza Hut',
+                price:2,
+                favorite:true
+            }
+        ]
+
+        //we need to convert the array of restaurants into an ember object so we can get() and set() later
+        const arrObjs = arr.map(a => {
+            return Ember.Object.create(a)
+        });
+
+        return arrObjs;
+
     }
 });
